5543d725cef3a6e42bd16c5bab415d707859665b
EllenZhangYarong <ellenzhg@hotmail.com>
2018-05-10 09:46:27 +1200

    Create Guesture recognize project

---
+++ GuestureClassifier/GuestureClassifier/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GuestureClassifier
+//
+//  Created by 张亚荣 on 2018/5/10.
+//  Copyright © 2018年 EllenAndAngel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
